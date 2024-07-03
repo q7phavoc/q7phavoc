6df940f2d0e72b66e57dafa890693d2406bb366a
John <q7phavoc@gmail.com>
2024-07-03 14:23:36 +0900

    class component

--- samples/fake/fake/src/App.tsx
+++ samples/fake/fake/src/App.tsx
@@ -1,13 +1,32 @@
-import * as D from "./data";
+// import * as D from "./data";
 
-export default function App() {
-  return (
-    <div>
-      <p>
-        {D.randomName()}, {D.randomJobTitle()}, {D.randomDayMonthYear()}
-      </p>
-      <img src={D.randomAvatar()} height="50" />
-      <img src={D.randomImage()} height="300" />
-    </div>
-  );
+// export default function App() {
+//   return (
+//     <div>
+//       <p>
+//         {D.randomName()}, {D.randomJobTitle()}, {D.randomDayMonthYear()}
+//       </p>
+//       <img src={D.randomAvatar()} height="50" />
+//       <img src={D.randomImage()} height="300" />
+//     </div>
+//   );
+// }
+
+import { Component } from "react";
+
+export default class App extends Component {
+  render() {
+    const isLoading = true;
+    if (isLoading) return <p>loading...</p>;
+
+    return (
+      <ul>
+        <li>
+          <a href="http://www.google.com">
+            <p>go to google</p>
+          </a>
+        </li>
+      </ul>
+    );
+  }
 }
