cef1d727395dd13de0adb1f1f05cd6337b31c5d3
John <q7phavoc@gmail.com>
2024-07-08 12:36:36 +0900

    css styling textstest

--- samples/fake/fake/src/pages/Color.tsx
+++ samples/fake/fake/src/pages/Color.tsx
@@ -3,7 +3,9 @@ export default function Color() {
     <div className="bg-sky-700">
       <p className="w-full p-4 text-3xl text-white">Color</p>
       <div className="mb-4">
-        <p className="text-white">Email Address</p>
+        <p className="text-lime-400 text-2xl font-bold italic text-justify">
+          Email Address
+        </p>
         <input
           type="email"
           className="text-gray-900 border-sky-200 border-4"
